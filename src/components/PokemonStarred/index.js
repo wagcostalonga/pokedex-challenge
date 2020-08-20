10fcf5ac177e2be2a929896a8dd6151262116177
Wagner Costalonga <costalongawagner@gmail.com>
2020-08-20 19:06:27 +0100

    Update index.js

--- src/components/PokemonStarred/index.js
+++ src/components/PokemonStarred/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft, FiX } from 'react-icons/fi';
